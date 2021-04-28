bc53de9a2773db88a9ee2a09be864d93254d70d3
Cheng LI <26736986+Leot6@users.noreply.github.com>
2021-04-28 22:06:38 +0800

    add code file structure

--- src/test_gitignore.cpp
+++ src/test_gitignore.cpp
@@ -10,7 +10,8 @@
 
 #include <libc.h>
 #include "gurobi_c++.h"
-
+#include <stdio.h>
+#include <iostream>
 #include <numeric>
 
 using namespace std;
@@ -35,53 +36,10 @@ int main(int argc, const char *argv[]) {
     auto path_to_config_file = root_directory + "/config/platform_demo.yml";
     auto platform_config = load_platform_config(path_to_config_file, root_directory);
 
-    try {
-
-        // Create an environment
-        GRBEnv env = GRBEnv(true);
-        env.set("LogToConsole", "0");
-        env.start();
-
-        // Create an empty model
-        GRBModel model = GRBModel(env);
-
-
-        // Create variables
-        // (lower_bound, upper_bounds, objective_coefficient (zero here and set later), variable_type, name)
-        std::vector<GRBVar> v_t;
-        for (auto i = 0; i < 3; i++) {
-            v_t.push_back(model.addVar(0.0, 1.0, 0.0, GRB_BINARY, fmt::format("vt_{}", i)));
-        }
-
-        // Set objective: maximize x + y + 2 z
-        std::vector<int> obj_c = {1, 1, 2};  // objective_coefficients
-        GRBLinExpr obj = 0.0;
-        for (auto i = 0; i < obj_c.size(); i++) {
-            obj += obj_c[i] * v_t[i];
-        }
-        model.setObjective(obj, GRB_MAXIMIZE);
-
-        // Add constraint for vehicles
-        std::vector<GRBLinExpr> c_v;
-        c_v.push_back(v_t[0] + 2 * v_t[1] + 3 * v_t[2]);
-        c_v.push_back(v_t[0] + v_t[1]);
-        model.addConstr(c_v[0] <= 4);
-        model.addConstr(c_v[1] >= 1);
-
-
-        // Optimize model
-        model.optimize();
-        for (const auto & var : v_t) {
-            fmt::print("{} {}\n", var.get(GRB_StringAttr_VarName), var.get(GRB_DoubleAttr_X));
-        }
-        fmt::print("Obj: {}\n", model.get(GRB_DoubleAttr_ObjVal));
-
-
-    } catch(GRBException e) {
-        cout << "Error code = " << e.getErrorCode() << endl;
-        cout << e.getMessage() << endl;
-    } catch(...) {
-        cout << "Exception during optimization" << endl;
+    for (int i = 0; i < 100; i++) {
+        DoProgress(i, 100); // 显示进度条
+        fflush(stdout);
+        sleep(1000); // 每次显示延迟1s
     }
 
     return 0;
